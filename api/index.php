39f1d79b4dbe5078365036040e053b056306e426
Cyrene <joedel.rf@gmail.com>
2026-05-18 01:29:10 +0800

    trylasttss

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Forward Vercel requests to the standard Laravel public/index.php
-require __DIR__ . '/../public/index.php';
+// require __DIR__ . '/../public/index.php';
 
 echo "PHP is working on Vercel!";
 phpinfo();
